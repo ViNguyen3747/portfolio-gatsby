4deddaa3f1fb2da487614369c9e4826dada4d8f4
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-07-29 19:14:03 -0500

    updated scrollbar

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,24 +3,23 @@ import * as styles from './layout.module.scss'
 import Header from './header/header'
 import Footer from './footer/footer'
 import { useThemeContext } from '../context/ThemeContext'
-import { scrollbar } from '../../styles/scrollbar.module.scss'
+import '../../styles/scrollbar.scss'
+
 const Layout = ({ children }) => {
     const [theme] = useThemeContext();
     return (
-        <body className={scrollbar}>
-            <div className={styles.container}>
-                <div className={theme === 'gloomy'
-                    ? styles.gloomy : null}>
-                    <div>
-                        <Header />
-                        <div className={styles.content}>
-                            {children}
-                        </div>
-                        <Footer />
+        <div className={styles.container}>
+            <div className={theme === 'gloomy'
+                ? styles.gloomy : null}>
+                <div>
+                    <Header />
+                    <div className={styles.content}>
+                        {children}
                     </div>
+                    <Footer />
                 </div>
             </div>
-        </body>
+        </div>
     )
 }
 
