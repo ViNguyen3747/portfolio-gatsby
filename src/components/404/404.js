df2df61efa1ffc8f015e0cfa057e1eab28aef361
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-07-30 19:27:53 -0500

    updated 404

---
+++ src/components/404/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as styles from './404.module.scss'
+const ErrorPage = () => (
+    <div className={styles.container}>
+        <span className={styles.letter}>4</span>
+        <span className={styles.letter}>0</span>
+        <span className={styles.letter}>4</span>
+        <div className={styles.subtitle}>PAGE NOT FOUND</div>
+    </div>
+)
+export default ErrorPage
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo"
-
+import ErrorPage from "../components/404/404"
 const NotFoundPage = () => (
     <Layout>
         <Seo title="404 Not found" />
-        <h1>Page not found</h1>
+        <ErrorPage />
     </Layout>
 )
 
