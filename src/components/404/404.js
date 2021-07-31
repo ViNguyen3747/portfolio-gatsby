db189b64165ad67268c31782885c338ed75097bf
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-07-30 20:00:10 -0500

    updated

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import * as styles from './404.module.scss'
+import { Link } from "gatsby"
+
 const ErrorPage = () => (
     <div className={styles.container}>
         <span className={styles.letter}>4</span>
         <span className={styles.letter}>0</span>
         <span className={styles.letter}>4</span>
         <div className={styles.subtitle}>PAGE NOT FOUND</div>
+        <Link to='/' className={styles.subtitle}>Back to Home</Link>
     </div>
 )
 export default ErrorPage
